17a16ae269e480e4a475ebb8bf6ece988c84bd25
Orche <orkv1234@gmail.com>
2025-06-21 22:17:29 +0300

    final for now

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+  </React.StrictMode>
+);
